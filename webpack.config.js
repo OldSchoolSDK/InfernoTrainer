17265faf12c9454f025e9957142131e195524b35
Tesla Owner <oldschoolsdk@gmail.com>
2021-07-24 18:17:41 -0600

    fully disable

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
