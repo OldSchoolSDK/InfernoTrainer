6f448abe8b2da1be9dba843a561cb40058c033bf
Tesla Owner <oldschoolsdk@gmail.com>
2021-07-30 22:41:18 -0600

    viewport dynamic

--- src/sdk/World.ts
+++ src/sdk/World.ts
@@ -82,6 +82,12 @@ export class World {
     this.viewport = document.getElementById(selector) as HTMLCanvasElement;
 
 
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    this._viewport.width = Math.floor(width / Settings.tileSize  - (204 / Settings.tileSize) )
+    this._viewport.height = Math.floor(height / Settings.tileSize  - (70 / Settings.tileSize) )
+    
     // create new canvas that is the on screen canvas
     this.viewport.width = Settings.tileSize * this._viewport.width + this.mapController.width;
     this.viewport.height = Settings.tileSize * this._viewport.height
