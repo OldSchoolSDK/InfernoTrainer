9e1e1a0cf3643bc30b9eef4ee3e4c49ea2e91d7a
Tesla Owner <oldschoolsdk@gmail.com>
2021-08-03 21:58:33 -0600

    jad also has a delay

--- src/content/inferno/js/mobs/TzKalZuk.ts
+++ src/content/inferno/js/mobs/TzKalZuk.ts
@@ -95,7 +95,7 @@ export class TzKalZuk extends Mob {
         this.setTimer += 175;
         this.timerPaused = false;
         // Spawn Jad
-        const jad = new JalTokJad(this.world, { x: 24, y: 25}, { aggro: this.shield, attackSpeed: 8, stun: 1, healers: 3, isZukWave: true });
+        const jad = new JalTokJad(this.world, { x: 24, y: 25}, { aggro: this.shield, attackSpeed: 8, stun: 1, healers: 3, isZukWave: true, spawnDelay: 5 });
         this.world.addMob(jad)
       }  
     }
