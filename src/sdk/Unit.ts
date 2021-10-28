63f6b07b99d25d2050670e11f15fe8086c87a6d6
Tesla Owner <oldschoolsdk@gmail.com>
2021-10-27 23:07:58 -0600

    Sets can be 1 tick apart, proving it resets only if you're under half [still unsure about the rounding in the condition, but the setting is correct]

--- src/sdk/Unit.ts
+++ src/sdk/Unit.ts
@@ -375,7 +375,7 @@ export class Unit extends GameObject {
         if (this.shouldChangeAggro(projectile)) {
           this.setAggro(projectile.from);
 
-          if (this.attackCooldownTicks <= 0) {
+          if (this.attackCooldownTicks < Math.floor(this.cooldown / 2)){
             this.attackCooldownTicks = Math.floor(this.cooldown / 2);
           }
         }
