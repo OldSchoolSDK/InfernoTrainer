66ee6c7f4415c468481ae4bb4756dfe56d923db1
Tesla Owner <oldschoolsdk@gmail.com>
2021-10-27 18:52:56 -0600

    jad spawn delay during zuk might actually be 9

--- src/content/inferno/js/mobs/TzKalZuk.ts
+++ src/content/inferno/js/mobs/TzKalZuk.ts
@@ -131,7 +131,7 @@ export class TzKalZuk extends Mob {
         this.setTimer += 175;
         this.timerPaused = false;
         // Spawn Jad
-        const jad = new JalTokJad(this.world, { x: 24, y: 25}, { aggro: this.shield, attackSpeed: 8, stun: 1, healers: 3, isZukWave: true, spawnDelay: 8 });
+        const jad = new JalTokJad(this.world, { x: 24, y: 25}, { aggro: this.shield, attackSpeed: 8, stun: 1, healers: 3, isZukWave: true, spawnDelay: 9 });
         this.world.region.addMob(jad)
       }  
     }
