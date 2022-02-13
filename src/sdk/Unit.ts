b91855f50ddc74cc9e0cee1a35a1513aa70a1c9c
Tesla Owner <oldschoolsdk@gmail.com>
2022-02-13 11:46:08 -0500

    Color code attacks

--- src/sdk/Unit.ts
+++ src/sdk/Unit.ts
@@ -548,7 +548,17 @@ export class Unit extends GameObject {
         );
       }else{
         this.world.region.context.beginPath()
+
         this.world.region.context.fillStyle = '#D1BB7773'
+        console.log('style', projectile.attackStyle);
+        if (projectile.attackStyle === 'slash' || projectile.attackStyle === 'crush' || projectile.attackStyle === 'stab') {
+          this.world.region.context.fillStyle = '#FF000073';
+        }else if (projectile.attackStyle === 'range') {
+          this.world.region.context.fillStyle = '#00FF0073';
+
+        }else if (projectile.attackStyle === 'magic') {
+          this.world.region.context.fillStyle = '#0000FF73';
+        }
         this.world.region.context.arc(0, 0, 5, 0, 2 * Math.PI)
         this.world.region.context.fill()
       }
