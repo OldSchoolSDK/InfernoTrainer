099d19d665bdecf1c93e32884bac6197a6b0d9dc
Supalosa <supalosa@supalosa.com>
2024-05-10 19:44:00 +1000

    fix test

--- test/simulations/Combat.test.ts
+++ test/simulations/Combat.test.ts
@@ -6,9 +6,11 @@ import { Random } from "../../src/sdk/Random";
 import { Viewport } from "../../src/sdk/Viewport";
 import { TestRegion } from "../utils/TestRegion";
 import { TestNpc } from "../utils/TestNpc";
+import { Settings } from "../../src";
 
 describe("basic combat scenario", () => {
-  test("when player tries to kill an fake jalxil...", () => {
+  test("when player tries to kill a fake jalxil...", () => {
+    Settings.inputDelay = 0;
     const region = new TestRegion(60, 60);
     const world = new World();
     region.world = world;
